aacc4ac1f26f75879828d70c84c7058f2cae1932
Max Sylver <raranmariano@hotmail.com>
2018-12-04 16:41:51 -0300

    header

--- app/src/main/java/me/maxcostadev/mhwcrowns/MainActivity.kt
+++ app/src/main/java/me/maxcostadev/mhwcrowns/MainActivity.kt
@@ -2,11 +2,28 @@ package me.maxcostadev.mhwcrowns
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val NUMBER_OF_MONSTERS = 99
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadData()
     }
+
+    fun loadData(){
+        //TODO: load the data
+        setUpProgressBar()
+    }
+
+    fun setUpProgressBar(){
+        mc_progress.max = NUMBER_OF_MONSTERS
+        bc_progress.max = NUMBER_OF_MONSTERS
+        mc_progress.progress = 86
+        bc_progress.progress = 35
+    }
+
 }
